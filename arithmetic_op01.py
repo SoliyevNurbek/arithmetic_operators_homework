be71d24d1bbec560f4bb50d7a8cfa93335b37a52
SoliyevNurbek <soliyevnurbek4243@gmail.com>
2023-09-23 08:12:23 +0500

    misol1

--- arithmetic_op01.py
+++ arithmetic_op01.py
@@ -1,7 +1,4 @@
-#Create a variable 'x' and assign it the value of 10.
-
-#Create a variable 'y' and assign it the value of 5.
-
-#Create a variable 'result' and assign it the value of x + y.
-
-#Print the value of the 'result'.
+x=10
+y=5
+result=x+y
+print(result)
